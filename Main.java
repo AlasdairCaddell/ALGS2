fe8f5d0c6660daff60a708be3b20a3b0279a55fd
alasdair <alcaddell14@gmail.com>
2022-11-06 07:01:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import SuffixTreePackage.*;
+
+/**
+ * Main class - for accessing suffix tree applications
+ * @author David Manlove
+ */
+
+public class Main {
+
+	/**
+	 * The main method.
+	 * @param args the arguments
+	 * @return 
+	 */
+	
+	
+	public static SuffixTreeAppl helper(String file) {
+		FileInput one = new FileInput("file");
+		byte[] s = one.readFile();
+		SuffixTree tree = new SuffixTree(s);
+		SuffixTreeAppl appl = new SuffixTreeAppl(tree);
+		return appl;
+	}
+	
+	
+	public static void main(String args[]) {
+	        String errorMessage = "Required syntax:\n";
+                errorMessage += "  java Main SearchOne <filename> <query string> for Task 1\n";
+                errorMessage += "  java Main SearchAll <filename> <query string> for Task 2\n";
+                errorMessage += "  java Main LRS <filename> for Task 3\n" ;
+                errorMessage += "  java Main LCS <filename1> <filename2> for Task 4";
+
+		if (args.length < 2)
+			System.out.println(errorMessage);
+		else {
+			// get the command from the first argument
+			String command = args[0];
+
+			switch (command) {
+			case "SearchOne":
+				SuffixTreeAppl sTree = helper(args[1]);
+				Task1Info task1=sTree.searchSuffixTree(args[2].getBytes());
+				if(task1.getPos()<0) {
+					System.out.println("Search string \""+args[1]+"\" not found in "+args[2] );
+				}else {
+					System.out.println("Search string \""+args[1]+"\" found in "+args[2] );
+
+				}
+			case "SearchAll": {
+			        if (args.length < 3) {
+				        System.out.println(errorMessage);
+					break;
+				}
+				System.out.println("Search results should be displayed here");
+				break;
+			}
+			case "LRS": {
+				System.out.println("LRS results should be displayed here");
+				break;
+			}
+			case "LCS": {
+				if (args.length < 3) {
+					System.out.println(errorMessage);
+					break;
+				}
+				System.out.println("LCS results should be displayed here");
+				break;
+			}
+			default: System.out.println(errorMessage);
+			}
+		}
+	}
+}
---
+++ SuffixTreePackage/SuffixTreeAppl.java
@@ -0,0 +1,135 @@
+package SuffixTreePackage;
+
+/**
+ * Class with methods for carrying out applications of suffix trees
+ * @author David Manlove
+ */
+
+public class SuffixTreeAppl {
+
+	/** The suffix tree */
+	private SuffixTree t;
+
+	/**
+	 * Default constructor.
+	 */
+	public SuffixTreeAppl () {
+		t = null;
+	}
+	
+	/**
+	 * Constructor with parameter.
+	 * 
+	 * @param tree the suffix tree
+	 */
+	public SuffixTreeAppl (SuffixTree tree) {
+		t = tree;
+	}
+	
+	/**
+	 * Search the suffix tree t representing string s for a target x.
+	 * Stores -1 in Task1Info.pos if x is not a substring of s,
+	 * otherwise stores p in Task1Info.pos such that x occurs in s
+	 * starting at s[p] (p counts from 0)
+	 * - assumes that characters of s and x occupy positions 0 onwards
+	 * 
+	 * @param x the target string to search for
+	 * 
+	 * @return a Task1Info object
+	 */
+	public Task1Info searchSuffixTree(byte[] x) {
+		Task1Info task1Info = new Task1Info();
+		int pos, j, k;   //j:len to find
+		SuffixTreeNode current, next;
+		pos = 0;  // position in s
+		current = t.getRoot();
+		
+		while (true) {
+			// search for child of current with left label x such that t[x]==t[pos]
+					next = t.searchList(current.getChild(), x[pos]);
+				
+					if (next == null) {
+						//task1Info.setPos(-1);
+						return task1Info;
+					}
+					else {
+						j = current.getLeftLabel() + 1; //next char
+						k = pos + 1; //next string letter
+						while (j <= next.getRightLabel()) {
+							if (t.getString()[j] == x[k]) {
+								if(pos>=x.length) {
+									task1Info.setPos(j-x.length);
+									task1Info.setMatchNode(next);
+								}
+								j++;
+								k++;
+							}else {
+								break;
+							}
+							if (j > next.getRightLabel()) {
+								// succeeded in matching whole segment, so go further down tree
+								pos = k;
+								current = next;
+							}
+							
+						}
+					}
+	
+			
+			
+			break;
+		}
+		return task1Info;
+	}
+
+	/**
+	 * Search suffix tree t representing string s for all occurrences of target x.
+	 * Stores in Task2Info.positions a linked list of all such occurrences.
+	 * Each occurrence is specified by a starting position index in s
+	 * (as in searchSuffixTree above).  The linked list is empty if there
+	 * are no occurrences of x in s.
+	 * - assumes that characters of s and x occupy positions 0 onwards
+	 * 
+	 * @param x the target string to search for
+	 * 
+	 * @return a Task2Info object
+	 */
+	public Task2Info allOccurrences(byte[] x) {
+
+		return null; // replace with your code!
+	}
+
+	/**
+	 * Traverses suffix tree t representing string s and stores ln, p1 and
+	 * p2 in Task3Info.len, Task3Info.pos1 and Task3Info.pos2 respectively,
+	 * so that s[p1..p1+ln-1] = s[p2..p2+ln-1], with ln maximal;
+	 * i.e., finds two embeddings of a longest repeated substring of s
+	 * - assumes that characters of s occupy positions 0 onwards
+	 * so that p1 and p2 count from 0
+	 * 
+	 * @return a Task3Info object
+	 */
+	public Task3Info traverseForLrs () {
+		
+		return null; // replace with your code!
+	}
+
+	/**
+	 * Traverse generalised suffix tree t representing strings s1 (of length
+	 * s1Length), and s2, and store ln, p1 and p2 in Task4Info.len,
+	 * Task4Info.pos1 and Task4Info.pos2 respectively, so that
+	 * s1[p1..p1+ln-1] = s2[p2..p2+ln-1], with len maximal;
+	 * i.e., finds embeddings in s1 and s2 of a longest common substring 
+         * of s1 and s2
+	 * - assumes that characters of s1 and s2 occupy positions 0 onwards
+	 * so that p1 and p2 count from 0
+	 * 
+	 * @param s1Length the length of s1
+	 * 
+	 * @return a Task4Info object
+	 */
+	public Task4Info traverseForLcs (int s1Length) {
+		
+		return null; // replace with your code!
+	}
+}
